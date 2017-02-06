059bae9f3d480507dd9ba53cc6dc3168e9ea9993
MKS-Student <mks.student@hackreactor.com>
2017-02-06 13:12:21 -0800

    Completed Forager bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,15 +1,19 @@
-var ForagerBee = function(age, color, food, job, canFly) {
-  Bee.call(this, age, color, food, job);
-  this.canFly = canFly;
+var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.canFly = true;
+  this.job = 'find pollen';
   this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(Grub.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 };
 
-ForagerBee.prototype = Object.create(Grub.prototype);
-ForagerBee.prototype.constructor = ForagerBee;
+
 
 // var fBuzz = new ForagerBee(10, yellow, jelly, 'find pollen', true)
 
