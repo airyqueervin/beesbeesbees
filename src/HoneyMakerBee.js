9c04eaa3026b6d73787805aede6371f261aa4c9f
MKS-Student <mks.student@hackreactor.com>
2017-02-06 13:11:53 -0800

    Completed Honeymaker bee and pass all tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,14 +1,22 @@
-var HoneyMakerBee = function(age, color, food, job, honeyPot) {
-  Bee.call(this, age, color, food, job);
-  this.honeyPot = honeyPot;
+var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
-HoneyMakerBee.prototype.makeHoney = function(honeyPot) {
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+
+HoneyMakerBee.prototype.makeHoney = function() {
   this.honeyPot++;
 };
 
-HoneyMakerBee.prototype.giveHoney = function(honeyPot) {
+HoneyMakerBee.prototype.giveHoney = function() {
   this.honeyPot--;
 }
+
+
 
 // var hBuzz = new HoneyMakerBee(10, yellow, jelly, 'make honey', 0)
